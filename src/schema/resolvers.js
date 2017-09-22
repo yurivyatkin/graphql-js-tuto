ddbc78da0c248bf23e347800a0c650d3f454f29a
Yuri Vyatkin <yuri.vyatkin@users.noreply.github.com>
2017-09-22 13:50:18 +1200

    Authentication - Create Users

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -10,6 +10,17 @@ module.exports = {
       const response = await Links.insert(data); // 3
       return Object.assign({id: response.insertedIds[0]}, data); // 4
     },
+    createUser: async (root, data, {mongo: {Users}}) => {
+    // You need to convert the given arguments into the format for the
+    // `User` type, grabbing email and password from the "authProvider".
+    const newUser = {
+        name: data.name,
+        email: data.authProvider.email.email,
+        password: data.authProvider.email.password,
+    };
+    const response = await Users.insert(newUser);
+    return Object.assign({id: response.insertedIds[0]}, newUser);
+    },
   },
 
   Link: {
