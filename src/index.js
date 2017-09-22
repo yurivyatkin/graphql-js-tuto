82d090d09db837d086f64042bcc5cffa0b5ef1a5
Yuri Vyatkin <yuri.vyatkin@users.noreply.github.com>
2017-09-22 13:11:45 +1200

    Connectors
    https://www.howtographql.com/graphql-js/4-connectors/

--- src/index.js
+++ src/index.js
@@ -9,14 +9,27 @@ const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
 
 const schema = require('./schema');
 
-var app = express();
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+// 1
+const connectMongo = require('./mongo-connector');
 
-app.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
-}));
+// 2
+const start = async () => {
+  // 3
+  const mongo = await connectMongo();
+  var app = express();
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
+    context: {mongo}, // 4
+    schema
+  }));
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+  }));
 
-const PORT = 3000
-app.listen(PORT, () => {
-  console.log(`Hackernews GraphQL server running on port ${PORT}.`)
-});
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Hackernews GraphQL server running on port ${PORT}.`)
+  });
+};
+
+// 5
+start();
